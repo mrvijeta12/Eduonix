a4930d129a68f14423f75640a28fbbe3c6ccec69
vijeta <vijetavarma1@gmail.com>
2023-10-29 09:48:19 +0530

    DOM 2 Assignment

---
+++ Vijeta_Varma_DOM_2_29_Oct/main.js
@@ -0,0 +1,29 @@
+async function data() {
+    let response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false");
+
+    const details = await response.json();
+    return details;
+}
+
+data().then((objectData) => {
+    let tableData = "";
+    objectData.map((values) => {
+        tableData += `<tr>
+        <td> <img src=" ${values.image}"/><td>
+        <td> ${values.name}<td>
+        <td> ${values.id}<td>
+        <td> ${values.symbol}<td>
+        <td> ${values.current_price}<td>
+        <td> ${values.total_volume}<td>`
+    });
+
+    document.getElementById("crypto-table").innerHTML = tableData
+
+
+
+
+
+}).catch((error) => {
+    console.log(error);
+});
+
